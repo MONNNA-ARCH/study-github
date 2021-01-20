fcd94b056c0c7ec1139421cc998567d85ce8ed2d
MONNNA-ARCH <chaomeng.ccm@gmail.com>
2021-01-20 15:15:01 -0500

    added image

---
+++ script.js
@@ -0,0 +1,33 @@
+$( document).ready (function(){
+	//Hover effect for H1 ELEMENT
+	$ (":JUMBOTRON H1") . hover (function() {
+	$ (This ).css({
+
+	  "color": "teal"
+	  "background-color": "gold")
+	});
+
+	$("div.jumbotron").click(function(){
+		 $(this).find("p").css("color","red"):
+	})
+
+	$(".card-img-top").click(function(){
+		$(this),parents(".card").find(",card-body").hide();
+		$(this),parents(".card").find(",card-body").toggle();
+		$(this),parents(".card").find(",card-body").fadeToggle(3000);
+		$(this),parents(".card").find(",card-body").slideToggle(3000);
+	});
+
+
+	$("#more-cats").click(function(){
+      for (i=0; i<8; i++) {
+      	 console.log("i equals "+ i);
+      }
+		$("#cats-container").append("<p>More Cats<p>")
+	})
+	
+	$(window).scroll(function(){
+		
+	})
+}
+
